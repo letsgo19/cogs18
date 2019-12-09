d691dc88e0e94c09ffd5a6bf03b6e639a03aec27
David <daykong119@gmail.com>
2019-12-09 01:12:38 -0800

    second push

--- cogs18 Final Project.py
+++ cogs18 Final Project.py
@@ -1,5 +1,6 @@
 import discord  #디스코드 불러오는것
 import urllib.request   #웹사이트 읽을려고
+import nltk
 from bs4 import BeautifulSoup  # web scraping 해서 필요한것들만 찾으려는 작업 web data extraction
 
 client = discord.Client() # 디스코드 셋업하기 위하는 용도
@@ -12,7 +13,6 @@ class DiscordBot:
         print('ready')
         game= discord.Game('test_bot') #봇 상태를 알려주기 위해서 재미로 넣음
         await client.change_presence(status=discord.Status.online, activity=game)
-
 
     #봇이 온라인 상태일떄 이 메세지를 받고 응답하고 아니면 대답안함.
     @client.event
@@ -51,9 +51,8 @@ class DiscordBot:
         if message.content.startswith('connect'):
             await client.change_presence(status=discord.Status.online)
 
-
-
-    client.run('NjUxMzQ3MzE3NjMwMTczMTg1.XeYklQ.-A7QusMz-oCW4ZJHTkf7K1wmKKg') # 커넥션할때 토큰 써야함
+    file_content = open("secret.txt").read()
+    client.run(file_content)        # 커넥션할때 토큰 써야함
 
 bot = DiscordBot() # 이 클라스를 실행하라
 
